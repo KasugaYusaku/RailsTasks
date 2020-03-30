7b10b4af2425ed8848c72adfc235ba39a1507e05
春日優作 <yuayanao0105@gmail.com>
2020-03-30 06:24:34 +0000

    Rails04最新

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     validates :name, presence: true,  length: { maximum: 15}
     VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX}
-    VARID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8.32}+\z/
+    VARID_PASSWORD_REGEX = /\A(?=.*?[a-z])[a-z\d]{8,32}+\z/
     validates :password, presence: true, length: { in: 8..32}, format: { with: VARID_PASSWORD_REGEX}
     has_secure_password
 end
