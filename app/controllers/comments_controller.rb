c177c1bf2c69b886172e6a5fafb210f358f3b963
春日優作 <yuayanao0105@gmail.com>
2020-04-04 02:00:45 +0000

    Rails07改訂④

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,14 @@
 class CommentsController < ApplicationController
   def new
     @comment = Comment.new
+    @comment = current_user.comments.new
     @topic = Topic.find_by(params[:topic_id])
-    
     
   end
   
   def create
-    @comment = current_user.comments.new(comment_params)
-    @comment.user_id = current_user.id
-    @comment.topic_id = params[:topic_id]
+    @comment = current_user.comments.new(comment_params) 
+    
     
       if @comment.save
         redirect_to topics_path, success: "コメントに成功しました"
