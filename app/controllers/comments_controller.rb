8e4401d0df831b1c9417614dc7c683d28695f9ce
春日優作 <yuayanao0105@gmail.com>
2020-04-05 08:15:45 +0000

    eee

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def new
     @comment = Comment.new
     @comment = current_user.comments.new
+    @topic = Topic.find_by(params[:topic_id])
   end
   
   def create
